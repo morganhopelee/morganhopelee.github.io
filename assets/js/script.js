2de2edf52ad6e67bbe74337ffd6ad97eb3d1d391
morganhopelee <morganhopelee@gmail.com>
2016-04-26 23:30:26 -0700

    deleted some comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,26 +4,11 @@ $(document).ready(function(){
 	function parallax() {
 		var scrolled = $(window).scrollTop();
     	$(".moon-img").css("top", -(scrolled * 0.4) + "px");
-    	// so if you've scrolled 100 down the webpage, the parallax container moves 0.4 times the speed you've scrolled
     }
 
     $(window).scroll(function() {
         parallax();
     });
-
-    // function disappear() {
-    // 	var scrolled = $(window).scrollTop();
-    // 	$(".name").css("opacity", 1 - )
-    // }
-
-    // how to fade out more smoothly?
- //    $('.name').waypoint(function(direction){
-	// 	if (direction == "down") {
-	// 		$('.name').fadeOut(1000);
-	// 	} else {
-	// 		$('.name').fadeIn(1000);
-	// 	}
-	// });
 
 	/* Fixed Navbar Function */
 	$('.content-container').waypoint(function(direction) {
